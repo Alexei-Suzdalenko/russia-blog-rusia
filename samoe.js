6fa916c6cdcc2f61cc84402268609321266f3802
Saron - Santa Maria de Cayon <75933673+Alexei-Suzdalenko@users.noreply.github.com>
2021-09-05 13:00:00 +0200

    Update samoe.js

--- samoe.js
+++ samoe.js
@@ -1,4 +1,5 @@
 [
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCV_n_D4KwuFXrBol8dWOUvQ',
   'https://www.youtube.com/feeds/videos.xml?channel_id=UCybBdfOZC_NwKJholfWfrTA',
   'https://www.youtube.com/feeds/videos.xml?channel_id=UCQkBKu3VZ6d96KJz_ixCq4g'
 ]
